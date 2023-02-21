dd6288550ef58ba3bea931a3b2e444d077af76a6
Victor Pan <41593388+LordArugula@users.noreply.github.com>
2023-02-21 12:49:13 -0800

    finished codelab 02.2
    Added lifecycle callbacks
    Added savedInstanceState to save replies

--- app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -12,12 +13,17 @@ public class SecondActivity extends AppCompatActivity {
 
     public static final String EXTRA_REPLY = "com.example.twoactivities.extra.REPLY";
 
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
+
     private EditText mReply;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Log.d(LOG_TAG, "-------");
+        Log.d(LOG_TAG, "onCreate");
 
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
@@ -28,6 +34,42 @@ public class SecondActivity extends AppCompatActivity {
         mReply = findViewById(R.id.editText_second);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
+
     public void returnReply(View view) {
 
         String reply = mReply.getText().toString();
@@ -35,6 +77,8 @@ public class SecondActivity extends AppCompatActivity {
         Intent replyIntent = new Intent(this, MainActivity.class);
         replyIntent.putExtra(EXTRA_REPLY, reply);
         setResult(RESULT_OK, replyIntent);
+
+        Log.d(LOG_TAG, "End SecondActivity");
 
         finish();
     }
